e578765af923714b6dff273a31e085a67656343a
heni98 <46896515+heni98@users.noreply.github.com>
2019-03-04 03:20:10 +0100

    Add files via upload
    projet final

---
+++ readme.sh
@@ -0,0 +1,32 @@
+Projet scripting concernant les caracteristiques hardware de la machine  :
+
+
+L'application contient :
+
+Un menu textuel
+
+Un menu graphique
+
+Les commandes :
+
+./projet.sh -g :: Lancer le menu grahique
+
+./projet.sh -t :: Lancer le menu textuel
+
+./projet.sh -ltop :: lancer la commande top 
+
+./projet.sh -lps :: lancer la commande ps -aux
+
+./projet.sh -root :: afficher les processus lances par l'utilisateur root 
+
+./projet.sh -mcpu :: afficher les processus les plus gourmands en termes cpu 
+
+./projet.sh -help :: afficher le help 
+
+
+Les fichiers :
+
+les resultats de la 1ere question "a partir de menu graphique" sont stockes dans le fichier FTOP 
+
+
+//projet realisé par Firas Mechmech et Heni Hcini 2A3
